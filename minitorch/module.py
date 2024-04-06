c166683f74288a25902ca2887908955c18a2e9b5
noahi <noah.i.larson@gmail.com>
2024-04-06 17:10:56 -0500

    commit

--- minitorch/module.py
+++ minitorch/module.py
@@ -1,88 +1,95 @@
+from __future__ import annotations
+
+from typing import Any, Dict, Optional, Sequence, Tuple
+
+
 class Module:
     """
     Modules form a tree that store parameters and other
     submodules. They make up the basis of neural network stacks.
 
     Attributes:
-        _modules (dict of name x :class:`Module`): Storage of the child modules
-        _parameters (dict of name x :class:`Parameter`): Storage of the module's parameters
-        training (bool): Whether the module is in training mode or evaluation mode
+        _modules : Storage of the child modules
+        _parameters : Storage of the module's parameters
+        training : Whether the module is in training mode or evaluation mode
 
     """
 
-    def __init__(self):
+    _modules: Dict[str, Module]
+    _parameters: Dict[str, Parameter]
+    training: bool
+
+    def __init__(self) -> None:
         self._modules = {}
         self._parameters = {}
         self.training = True
 
-    def modules(self):
+    def modules(self) -> Sequence[Module]:
         "Return the direct child modules of this module."
-        return self.__dict__["_modules"].values()
+        m: Dict[str, Module] = self.__dict__["_modules"]
+        return list(m.values())
 
-    def train(self):
+    def train(self) -> None:
         "Set the mode of this module and all descendent modules to `train`."
-
         def _train(module):
             module.training = True
             for m in module.modules():
                 _train(m)
-
-        _train(self)
-
-    def eval(self):
+        return _train(m)
+    
+    def eval(self) -> None:
         "Set the mode of this module and all descendent modules to `eval`."
-
         def _eval(module):
             module.training = False
             for m in module.modules():
                 _eval(m)
+        return eval(self)
+       
 
-        _eval(self)
-
-    def named_parameters(self):
+    def named_parameters(self) -> Sequence[Tuple[str, Parameter]]:
         """
         Collect all the parameters of this module and its descendents.
 
 
         Returns:
-            list of pairs: Contains the name and :class:`Parameter` of each ancestor parameter.
+            The name and `Parameter` of each ancestor parameter.
         """
+        if len(self._modules) == 0:
+            new_dict = {}
+            for p in self._parameters:
+                new_dict[p] = self._parameters[p]
+            return new_dict
+        else:
+            this_para = {}
+            for key in self._parameters:
+                this_para[key] = self._parameters[key]
+            for key in self._modules:
+                child_para = self._modules[key].named_parameters()
+                for name in child_para:
+                    this_para[key + "." + name] = child_para[name]
+            return this_para
 
-        def _named_parameters(module, prefix=""):
-            for name, param in module._parameters.items():
-                yield prefix + name, param
-            for name, module in module._modules.items():
-                yield from _named_parameters(module, prefix + name + ".")
-
-        return list(_named_parameters(self))
-
-    def parameters(self):
+    def parameters(self) -> Sequence[Parameter]:
         "Enumerate over all the parameters of this module and its descendents."
 
-        # def _parameters(module):
-        #     for param in module._parameters.values():
-        #         yield param
-        #     for module in module._modules.values():
-        #         yield from _parameters(module)
+        return self.named_parameters().values()
 
-        return [param for _, param in self.named_parameters()]
-
-    def add_parameter(self, k, v):
+    def add_parameter(self, k: str, v: Any) -> Parameter:
         """
         Manually add a parameter. Useful helper for scalar parameters.
 
         Args:
-            k (str): Local name of the parameter.
-            v (value): Value for the parameter.
+            k: Local name of the parameter.
+            v: Value for the parameter.
 
         Returns:
-            Parameter: Newly created parameter.
+            Newly created parameter.
         """
         val = Parameter(v, k)
         self.__dict__["_parameters"][k] = val
         return val
 
-    def __setattr__(self, key, val):
+    def __setattr__(self, key: str, val: Parameter) -> None:
         if isinstance(val, Parameter):
             self.__dict__["_parameters"][key] = val
         elif isinstance(val, Module):
@@ -90,27 +97,25 @@ class Module:
         else:
             super().__setattr__(key, val)
 
-    def __getattr__(self, key):
+    def __getattr__(self, key: str) -> Any:
         if key in self.__dict__["_parameters"]:
             return self.__dict__["_parameters"][key]
 
         if key in self.__dict__["_modules"]:
             return self.__dict__["_modules"][key]
+        return None
 
-    def __call__(self, *args, **kwargs):
+    def __call__(self, *args: Any, **kwargs: Any) -> Any:
         return self.forward(*args, **kwargs)
 
-    def forward(self):
-        assert False, "Not Implemented"
-
-    def __repr__(self):
-        def _addindent(s_, numSpaces):
-            s = s_.split("\n")
-            if len(s) == 1:
+    def __repr__(self) -> str:
+        def _addindent(s_: str, numSpaces: int) -> str:
+            s2 = s_.split("\n")
+            if len(s2) == 1:
                 return s_
-            first = s.pop(0)
-            s = [(numSpaces * " ") + line for line in s]
-            s = "\n".join(s)
+            first = s2.pop(0)
+            s2 = [(numSpaces * " ") + line for line in s2]
+            s = "\n".join(s2)
             s = first + "\n" + s
             return s
 
@@ -139,7 +144,7 @@ class Parameter:
     any value for testing.
     """
 
-    def __init__(self, x=None, name=None):
+    def __init__(self, x: Any, name: Optional[str] = None) -> None:
         self.value = x
         self.name = name
         if hasattr(x, "requires_grad_"):
@@ -147,7 +152,7 @@ class Parameter:
             if self.name:
                 self.value.name = self.name
 
-    def update(self, x):
+    def update(self, x: Any) -> None:
         "Update the parameter value."
         self.value = x
         if hasattr(x, "requires_grad_"):
@@ -155,8 +160,8 @@ class Parameter:
             if self.name:
                 self.value.name = self.name
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return repr(self.value)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return str(self.value)
